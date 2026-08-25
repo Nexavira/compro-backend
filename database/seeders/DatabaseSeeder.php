56662460b0a9ce2cef553c467392084c5a8162b8
Pranesha Wahyu Sinatria Aldiarta <praneshawahyu@gmail.com>
2026-08-25 21:02:02 +0700

    fix: disable global template seeders, add tenant_templates, adjust columns on cms migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             TenantCategorySeeder::class,
-            GlobalTemplateSeeder::class,
-            GlobalTemplateAboutPageSeeder::class,
+            // GlobalTemplateSeeder::class,
+            // GlobalTemplateAboutPageSeeder::class,
             TenantSeeder::class,
             UserSeeder::class,
             PermissionSeeder::class,
